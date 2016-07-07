a8c7e14233136c864084afd80dafd937792d28fb
Jonas Holbech <jh@jonasholbech.dk>
2016-07-08 00:19:53 +0200

    removes double header

--- app/js/Ajax.js
+++ app/js/Ajax.js
@@ -3,6 +3,7 @@ var Ajax = {
         var xobj = new XMLHttpRequest();
         xobj.open('POST', path, true);
         xobj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xobj.setRequestHeader("X-Requested-With", "XMLHttpRequest");
         var s=[];
         for (var prop in dataObj) {
             if(dataObj.hasOwnProperty(prop)){
@@ -20,6 +21,7 @@ var Ajax = {
     get:function (path, callback) {
         var xobj = new XMLHttpRequest();
         xobj.open('GET', path, true);
+        xobj.setRequestHeader("X-Requested-With", "XMLHttpRequest");
         xobj.onreadystatechange = function () {
             if (xobj.readyState == 4 && xobj.status == "200") {
                 callback(xobj.responseText);
@@ -31,6 +33,7 @@ var Ajax = {
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
         xobj.open('GET', path, true);
+        xobj.setRequestHeader("X-Requested-With", "XMLHttpRequest");
         xobj.onreadystatechange = function () {
             if (xobj.readyState == 4 && xobj.status == "200") {
                 callback(JSON.parse(xobj.responseText));
